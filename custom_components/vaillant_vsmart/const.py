4629665b2ba8850ffd524d1de4bb200a3cecdf31
albaintor <118518828+albaintor@users.noreply.github.com>
2024-06-27 21:21:03 +0200

    Added gaz consumption sensors (#321)

--- custom_components/vaillant_vsmart/const.py
+++ custom_components/vaillant_vsmart/const.py
@@ -1,4 +1,6 @@
 """Constants for Vaillant vSMART."""
+from homeassistant.components.sensor import SensorEntityDescription, SensorDeviceClass
+from vaillant_netatmo_api import MeasurementType, Device, Module
 
 # Base component constants
 NAME = "Vaillant vSMART"
@@ -15,3 +17,57 @@ PLATFORMS = [CLIMATE, SELECT, SENSOR, SWITCH, WATER_HEATER]
 # Configuration and options
 CONF_APP_VERSION = "app_version"
 CONF_USER_PREFIX = "user_prefix"
+
+
+# Sensor entity descriptions for measurements
+class VaillantSensorEntityDescription:
+    def __init__(self, key: str, measurement_type: MeasurementType, sensor_name: str, device_class: SensorDeviceClass,
+                 icon: str,
+                 unit: str, conversion: float = 1, enabled=True,
+                 device: Device = None,
+                 module: Module = None):
+        self.key = key
+        self.measurement_type = measurement_type
+        self.sensor_name: str = sensor_name
+        self.device_class = device_class
+        self.icon = icon
+        self.unit = unit
+        self.conversion = conversion
+        self.enabled = enabled
+        self.device = device
+        self.module = module
+
+
+# List of measurement sensors to create
+MEASUREMENT_SENSORS: list[VaillantSensorEntityDescription] = [
+    VaillantSensorEntityDescription(key="gaz_heating",
+                                    measurement_type=MeasurementType.SUM_ENERGY_GAS_HEATING,
+                                    sensor_name="Gaz heating",
+                                    icon="mdi:radiator",
+                                    device_class=SensorDeviceClass.ENERGY,
+                                    unit="kWh",
+                                    conversion=0.001,
+                                    enabled=True),
+    VaillantSensorEntityDescription(key="water_heating",
+                                    measurement_type=MeasurementType.SUM_ENERGY_GAS_WATER,
+                                    sensor_name="Gaz water heating",
+                                    icon="mdi:bathtub",
+                                    device_class=SensorDeviceClass.ENERGY,
+                                    unit="kWh",
+                                    conversion=0.001,
+                                    enabled=True),
+    VaillantSensorEntityDescription(key="elec_heating",
+                                    measurement_type=MeasurementType.SUM_ENERGY_ELEC_HEATING,
+                                    sensor_name="Electricity heating",
+                                    icon="mdi:meter-electric",
+                                    device_class=SensorDeviceClass.ENERGY,
+                                    unit="Wh",
+                                    enabled=False),
+    VaillantSensorEntityDescription(key="elec_water__heating",
+                                    measurement_type=MeasurementType.SUM_ENERGY_ELEC_WATER,
+                                    sensor_name="Electricity water heating",
+                                    icon="mdi:bathtub-outline",
+                                    device_class=SensorDeviceClass.ENERGY,
+                                    unit="Wh",
+                                    enabled=False)
+]
